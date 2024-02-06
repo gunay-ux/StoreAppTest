ad4fe8c5607e7120d3b0f15b229038751e42f19e
name.surname <name.surname@arcelik.com>
2024-02-06 16:25:14 +0300

    Driver added

--- src/main/java/implementation/CommenDriver.java
+++ src/main/java/implementation/CommenDriver.java
@@ -1,5 +1,35 @@
 package implementation;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+import java.net.MalformedURLException;
+
+
+import org.openqa.selenium.WebDriver;
+
+
 public class CommenDriver {
+	
+	public WebDriver driver;
+	
+	public CommenDriver() throws MalformedURLException {
+		
+		AppiumDriverLocalService service;
+		service = new AppiumServiceBuilder()
+		       .withIPAddress("127.0.0.1")
+		       .usingPort(4723)
+		       .build();
+		UiAutomator2Options option = new UiAutomator2Options() ;
+		option.setApp("/home/gunay/eclipse-workspace/StoreAppTest/src/test/resources/General-Store.apk");
+		option.setDeviceName("Myphone");
+	    driver  = new AndroidDriver(service.getUrl(), option);
+		
+		
+		
+	}
+	
 
 }
